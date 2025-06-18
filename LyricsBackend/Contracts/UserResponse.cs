c7f8867cd714c5ef6eb9054bd4a5f4325f446d54
BvBUndeader <dinko.gekov@gmail.com>
2025-06-18 20:04:21 +0300

    added user create and fetch endpoints

---
+++ LyricsBackend/Contracts/UserResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace LyricsBackend.Contracts
+{
+    public class UserResponse
+    {
+        public long Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
--- LyricsBackend/Program.cs
+++ LyricsBackend/Program.cs
@@ -1,3 +1,5 @@
+using LyricsBackend.Contracts;
+using LyricsBackend.Models;
 using Supabase;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,15 +11,15 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddScoped<Supabase.Client>(_ =>
-new Supabase.Client(
-    builder.Configuration["SupabaseUrl"],
-    builder.Configuration["SupabaseKey"],
-    new SupabaseOptions
-    {
-        AutoConnectRealtime = true
-    }
-    ));
+builder.Services.AddScoped(provider =>
+{
+    var url = builder.Configuration["Supabase:SupabaseUrl"];
+    var key = builder.Configuration["Supabase:SupabaseKey"];
+    var options = new SupabaseOptions { AutoConnectRealtime = false };
+    var client = new Supabase.Client(url, key, options);
+    client.InitializeAsync().Wait();
+    return client;
+});
 
 var app = builder.Build();
 
@@ -27,6 +29,47 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.MapPost("/users", async (
+    CreateUserRequest request, Supabase.Client client) =>
+{
+    var user = new Users
+    {
+        Username = request.Username,
+        Password = request.Password,
+        Email = request.Email
+    };
+
+    var response = await client.From<Users>().Insert(user);
+
+    var newUser = response.Models.First();
+
+    return Results.Ok(newUser.Id);
+});
+
+app.MapGet("/users/{id}", async (long id, Supabase.Client client) =>
+{
+    var response = await client.
+    From<Users>().Where(n => n.Id == id).Get();
+
+    var user = response.Models.FirstOrDefault();
+
+    if (user is null)
+    {
+        return Results.NotFound();
+    }
+
+    var userResponse = new UserResponse
+    {
+        Id = user.Id,
+        Username = user.Username,
+        Password = user.Password,
+        Email = user.Email,
+        CreatedAt = user.CreatedAt
+    };
+
+    return Results.Ok(userResponse);
+});
 
 app.UseHttpsRedirection();
 
